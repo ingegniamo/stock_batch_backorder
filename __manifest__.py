cfef64ed019cd46a8552e4e34b8f33f8d5d9ff67
Francesco Moccia <moccia.f@stesi.eu>
2022-02-21 16:35:01 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "",
+    
+    'summary': "",
+  
+    'license': 'OPL-1',
+
+    'author': "STeSI Srl",
+
+    'category': '',
+  
+    'version': '14.0.0.1',
+  
+    'website': "http://www.stesi.eu",
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+    
+    # always loaded
+    'data': ['security/ir.model.access.csv'],
+
+    'application': True,
+}
